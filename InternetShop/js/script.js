2ed54c9bd956b5b931b102a76706681e05102ef6
DmitryVolkov1997 <dmitrij_volkov_2014@mail.ru>
2020-09-05 20:02:19 +0600

    slider80

--- InternetShop/js/script.js
+++ InternetShop/js/script.js
@@ -21,6 +21,8 @@ testWebP(function (support) {
 
 $(function(){
   $('.banner-section__slider').slick({
-
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnpnext"><img src="img/arrow-right.svg" alt=""></button>'
   });
 });

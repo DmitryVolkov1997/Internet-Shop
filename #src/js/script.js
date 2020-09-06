a71d95eb26c63908d21a98518b99f253d668997f
DmitryVolkov1997 <dmitrij_volkov_2014@mail.ru>
2020-09-06 19:50:38 +0600

    tabs100

--- #src/js/script.js
+++ #src/js/script.js
@@ -16,12 +16,26 @@ testWebP(function (support) {
   }
 });
 //================END================
-@@include('slick.min.js');
+@@include("slick.min.js");
 
-$(function(){
-  $('.banner-section__slider').slick({
+$(function () {
+  $(".banner-section__slider").slick({
     dots: true,
-    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnpnext"><img src="img/arrow-right.svg" alt=""></button>'
+    prevArrow:
+      '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/arrow-left.svg" alt=""></button>',
+    nextArrow:
+      '<button class="banner-section__slider-btn banner-section__slider-btnpnext"><img src="img/arrow-right.svg" alt=""></button>',
   });
+
+  // Start tabs
+  $(".search__tabs-item").on("click", function (e) {
+    e.preventDefault();
+
+    $(".search__tabs-item").removeClass("search__tabs-item--active");
+    $(".search__content-item").removeClass("search__content-item--active");
+    
+    $(this).addClass('search__tabs-item--active');
+    $($(this).attr('href')).addClass('search__content-item--active');
+  });
+
 });
